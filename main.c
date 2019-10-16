6632633813aac7dca381062862e5946509a8ec1b
franek <franeklubi@gmail.com>
2019-10-16 16:00:34 +0200

    Add run and drawLines functions; Add test.scaf

--- main.c
+++ main.c
@@ -1,11 +1,55 @@
 #include <ncurses.h>
+#include <stdbool.h>
+
+#include "./scaffolding/fileHandling.h"
+#include "./scaffolding/Head.h"
+#include "./scaffolding/helperFunctions.h"
+#include "./scaffolding/interpreter.h"
+#include "./scaffolding/Lines.h"
+
+
+void drawLines() {
+
+}
+
+
+int run(char* filename) {
+    Head r_head = genHead();
+    Head w_head = genHead();
+    w_head.destructive = true;
+
+    FILE* file = fopen(filename, "r");
+        uint32_t file_size = getSize(file);
+        char* start_ptr = loadFile(file, file_size);
+    fclose(file);
+
+    Lines* lines_buffer = dissectLines(start_ptr, file_size);
+    free(start_ptr);
+    if ( !lines_buffer ) {
+        fprintf(stderr, "Error with creating lines_buffer\n");
+        return 1;
+    }
+
+    ProgramState state = genProgramState(lines_buffer, &r_head, &w_head);
+
+    bool should_end = false;
+
+    while ( !should_end ) {
+        should_end = execute(&state);
+        drawLines();
+        getch();
+    }
+
+    return 0;
+}
 
 
 int main() {
-
     initscr();
 
     refresh();
+
+    // run("./test.scaf");
 
     getch();
 
--- makefile
+++ makefile
@@ -1,6 +1,11 @@
 
 main: main.c
-	gcc -lncurses *.c
+	gcc -lncurses *.c \
+		./scaffolding/fileHandling.c \
+		./scaffolding/Head.c \
+		./scaffolding/helperFunctions.c \
+		./scaffolding/interpreter.c \
+		./scaffolding/Lines.c
 
 run:
 	make
