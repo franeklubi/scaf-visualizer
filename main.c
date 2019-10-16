2fd919626e3d286ce04615bf4d9d9747ac449632
franek <franeklubi@gmail.com>
2019-10-16 17:19:57 +0200

    Add drawHead

--- main.c
+++ main.c
@@ -10,30 +10,48 @@
 #include "./scaffolding/Lines.h"
 
 
-void drawLines(ProgramState* state) {
-    for ( uint32_t x = 0; x < state->buffer_ptr->no_lines; x++ ) {
+#define BLUE    1
+#define RED     2
+
+
+void drawHead(Head* head, uint32_t x_mod, uint8_t colour) {
+    mvchgat(
+        head->pos_y, head->pos_x+x_mod,
+        1, A_STANDOUT, colour, NULL
+    );
+}
+
+
+void drawLines(WINDOW* win, ProgramState* state) {
+    for ( uint32_t y = 0; y < state->buffer_ptr->no_lines; y++ ) {
         mvprintw(
-            x, 0,
+            y, 0,
             "[%x](%x):\t\"",
-            x, state->buffer_ptr->lines_len[x]
+            y, state->buffer_ptr->lines_len[y]
         );
 
-        for ( uint32_t i = 0; i < state->buffer_ptr->lines_len[x]; i++ ) {
+        // getting current cursor position for head drawing mod
+        int cur_y, cur_x;
+        getyx(win, cur_y, cur_x);
+
+        for ( uint32_t x = 0; x < state->buffer_ptr->lines_len[y]; x++ ) {
             printw(
                 "%c",
-                state->buffer_ptr->lines[x][i]
+                state->buffer_ptr->lines[y][x]
             );
         }
         printw("\"");
 
-        // mvchgat(0, x, 1, A_REVERSE, 0, NULL);
-        // mvchgat(0, 7, 1, A_REVERSE, 0, NULL);
+        drawHead(state->w_head_ptr, cur_x, RED);
+        drawHead(state->r_head_ptr, cur_x, BLUE);
     }
+
+
     refresh();
 }
 
 
-int run(char* filename) {
+int run(WINDOW* win, char* filename) {
     Head r_head = genHead();
     Head w_head = genHead();
     w_head.destructive = true;
@@ -56,7 +74,7 @@ int run(char* filename) {
 
     while ( !should_end ) {
         should_end = execute(&state);
-        drawLines(&state);
+        drawLines(win, &state);
         char c = getch();
         if ( c == 'q' ) {
             return 0;
@@ -70,13 +88,21 @@ int run(char* filename) {
 
 
 int main() {
-    initscr();
+    WINDOW* win = initscr();
+    start_color();
 
     noecho();
 
-    curs_set(false);
+    curs_set(FALSE);
 
-    run("./test.scaf");
+
+    if ( has_colors() != FALSE ) {
+        init_pair(BLUE, COLOR_BLUE, COLOR_BLACK);
+        init_pair(RED, COLOR_RED, COLOR_BLACK);
+    }
+
+
+    run(win, "./test.scaf");
 
     endwin();
 
